aedb643acf6f5770d61360876b6a555e362eb5b3
Emma K Alexandra <emma@emma.sh>
2020-01-21 12:35:20 -0500

    Bus client complete
    Add tests w/ vcr

--- lib/weese/bus/metro_bus.rb
+++ lib/weese/bus/metro_bus.rb
@@ -1,17 +1,42 @@
 # frozen_string_literal: true
 
 require 'weese/bus/stop'
+require 'weese/bus/route'
 
 module Weese
   module Bus
     # MetroBus client. Used for accessing MetroBus-related WMATA APIs.
     class MetroBus
+      include Requests::Requester
       include Bus::RequiresStop
+      include Bus::RequiresRoute
 
       attr_accessor :api_key
 
       def initialize(api_key)
         @api_key = api_key
+      end
+
+      def routes
+        fetch(
+          Requests::Request.new(
+            @api_key,
+            Bus::Urls::ROUTES,
+            {}
+          )
+        )
+      end
+
+      def stops(radius_at_coordinates = nil)
+        query = radius_at_coordinates ? radius_at_coordinates.to_h : {}
+
+        fetch(
+          Requests::Request.new(
+            @api_key,
+            Bus::Urls::STOPS,
+            query
+          )
+        )
       end
     end
   end
--- test/weese_test.rb
+++ test/weese_test.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require 'vcr'
 require 'test_helper'
 require 'weese/rail/metro_rail'
 require 'weese/bus/metro_bus'
 require 'weese/location'
 
 API_KEY = '9e38c3eab34c4e6c990828002828f5ed'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/fixtures'
+  config.hook_into :faraday
+end
 
 class WeeseTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -19,105 +25,133 @@ class MetroRailTest < Minitest::Test
   end
 
   def test_lines
-    assert_instance_of Hash, @client.lines
+    VCR.use_cassette('lines') do
+      assert_instance_of Hash, @client.lines
+    end
   end
 
   def test_entrances
-    assert_instance_of(
-      Hash,
-      @client.entrances(
-        Weese::Location::RadiusAtCoordinates.new(
-          1000,
-          Weese::Location::Coordinates.new(
-            38.8817596,
-            -77.0166426
+    VCR.use_cassette('entrances') do
+      assert_instance_of(
+        Hash,
+        @client.entrances(
+          Weese::Location::RadiusAtCoordinates.new(
+            1000,
+            Weese::Location::Coordinates.new(
+              38.8817596,
+              -77.0166426
+            )
           )
         )
       )
-    )
+    end
   end
 
   def test_positions
-    assert_instance_of Hash, @client.positions
+    VCR.use_cassette('positions') do
+      assert_instance_of Hash, @client.positions
+    end
   end
 
   def test_routes
-    assert_instance_of Hash, @client.routes
+    VCR.use_cassette('routes') do
+      assert_instance_of Hash, @client.routes
+    end
   end
 
   def test_circuits
-    assert_instance_of Hash, @client.circuits
+    VCR.use_cassette('circuits') do
+      assert_instance_of Hash, @client.circuits
+    end
   end
 
   def test_station_to_station
-    assert_instance_of(
-      Hash,
-      @client.station_to_station(
-        Weese::Rail::Station::A01,
-        Weese::Rail::Station::A02
+    VCR.use_cassette('station_to_station') do
+      assert_instance_of(
+        Hash,
+        @client.station_to_station(
+          Weese::Rail::Station::A01,
+          Weese::Rail::Station::A02
+        )
       )
-    )
+    end
   end
 
   def test_elevator_and_escalator_incidents
-    assert_instance_of(
-      Hash,
-      @client.elevator_and_escalator_incidents(
-        Weese::Rail::Station::A01
+    VCR.use_cassette('elevator_and_escalator_incidents') do
+      assert_instance_of(
+        Hash,
+        @client.elevator_and_escalator_incidents(
+          Weese::Rail::Station::A01
+        )
       )
-    )
+    end
   end
 
   def test_incidents
-    assert_instance_of(
-      Hash,
-      @client.incidents(Weese::Rail::Station::A01)
-    )
+    VCR.use_cassette('incidents') do
+      assert_instance_of(
+        Hash,
+        @client.incidents(Weese::Rail::Station::A01)
+      )
+    end
   end
 
   def test_next_trains
-    assert_instance_of(
-      Hash,
-      @client.next_trains(Weese::Rail::Station::A01)
-    )
+    VCR.use_cassette('next_trains') do
+      assert_instance_of(
+        Hash,
+        @client.next_trains(Weese::Rail::Station::A01)
+      )
+    end
   end
 
   def test_station_information
-    assert_instance_of(
-      Hash,
-      @client.station_information(Weese::Rail::Station::A01)
-    )
+    VCR.use_cassette('station_information') do
+      assert_instance_of(
+        Hash,
+        @client.station_information(Weese::Rail::Station::A01)
+      )
+    end
   end
 
   def test_parking_information
-    assert_instance_of(
-      Hash,
-      @client.parking_information(Weese::Rail::Station::A01)
-    )
+    VCR.use_cassette('parking_information') do
+      assert_instance_of(
+        Hash,
+        @client.parking_information(Weese::Rail::Station::A01)
+      )
+    end
   end
 
   def test_path_from
-    assert_instance_of(
-      Hash,
-      @client.path_from(
-        Weese::Rail::Station::A01,
-        Weese::Rail::Station::A02
+    VCR.use_cassette('path_from') do
+      assert_instance_of(
+        Hash,
+        @client.path_from(
+          Weese::Rail::Station::A01,
+          Weese::Rail::Station::A02
+        )
       )
-    )
+    end
   end
 
   def test_timings
-    assert_instance_of(
-      Hash,
-      @client.timings(Weese::Rail::Station::A01)
-    )
+    VCR.use_cassette('timings') do
+      assert_instance_of(
+        Hash,
+        @client.timings(Weese::Rail::Station::A01)
+      )
+    end
   end
 
   def test_stations
-    assert_instance_of(
-      Hash,
-      @client.stations(Weese::Rail::Line::BL)
-    )
+    VCR.use_cassette('stations') do
+      assert_instance_of(
+        Hash,
+        @client.stations(Weese::Rail::Line::BL)
+      )
+    end
   end
 end
 
@@ -127,16 +161,74 @@ class TestMetroBus < MiniTest::Test
   end
 
   def test_next_buses
-    assert_instance_of(
-      Hash,
-      @client.next_buses(Weese::Bus::Stop.new(1_001_195))
-    )
+    VCR.use_cassette('next_buses') do
+      assert_instance_of(
+        Hash,
+        @client.next_buses(Weese::Bus::Stop.new(1_001_195))
+      )
+    end
   end
 
   def test_stop_schedule
-    assert_instance_of(
-      Hash,
-      @client.stop_schedule(Weese::Bus::Stop.new(1_001_195))
-    )
+    VCR.use_cassette('stop_schedule') do
+      assert_instance_of(
+        Hash,
+        @client.stop_schedule(Weese::Bus::Stop.new(1_001_195))
+      )
+    end
+  end
+
+  def test_positions
+    VCR.use_cassette('bus_positions') do
+      assert_instance_of(
+        Hash,
+        @client.positions
+      )
+    end
+  end
+
+  def test_bus_incidents
+    VCR.use_cassette('bus_incidents') do
+      assert_instance_of(
+        Hash,
+        @client.incidents
+      )
+    end
+  end
+
+  def test_path
+    VCR.use_cassette('path') do
+      assert_instance_of(
+        Hash,
+        @client.path(Weese::Bus::Route::A12)
+      )
+    end
+  end
+
+  def test_route_schedule
+    VCR.use_cassette('route_schedule') do
+      assert_instance_of(
+        Hash,
+        @client.route_schedule(Weese::Bus::Route::A12, true)
+      )
+    end
+  end
+
+  def test_bus_routes
+    VCR.use_cassette('bus_routes') do
+      assert_instance_of(
+        Hash,
+        @client.routes
+      )
+    end
+  end
+
+  def test_stops
+    VCR.use_cassette('stops') do
+      assert_instance_of(
+        Hash,
+        @client.stops
+      )
+    end
   end
 end
